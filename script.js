5c3b44e257141f117f36626496813f6cea9799e2
Bhanuprakash6923 <158007213+Bhanuprakash6923@users.noreply.github.com>
2025-05-12 11:20:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 function fibonacci(num) {
-  if (num <= 0) {
-    return "Input must be a positive integer.";
-  }
-  if (num === 1) return 0;  // 1st Fibonacci number is 0
-  if (num === 2) return 1;  // 2nd Fibonacci number is 1
+    if (num === 1) return 0; // The 1st Fibonacci number
+    if (num === 2) return 1; // The 2nd Fibonacci number
 
-  let a = 0, b = 1;
-  for (let i = 3; i <= num; i++) {
-    let temp = a + b;
-    a = b;
-    b = temp;
-  }
-  return b;
+    let a = 0; // F(1)
+    let b = 1; // F(2)
+    let fib = 0;
+
+    for (let i = 3; i <= num; i++) {
+        fib = a + b; // F(n) = F(n-1) + F(n-2)
+        a = b; // Move to the next Fibonacci number
+        b = fib; // Update b to the current Fibonacci number
+    }
+
+    return fib; // Return the nth Fibonacci number
 }
 
-// Test cases
-console.log(fibonacci(1));  // Expected Output: 0
-console.log(fibonacci(2));  // Expected Output: 1
-console.log(fibonacci(5));  // Expected Output: 3
-console.log(fibonacci(10)); // Expected Output: 34
+module.exports = fibonacci;
